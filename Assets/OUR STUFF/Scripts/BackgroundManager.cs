c1e14710714f01c7b772c9079827aedcb3e99d62
elliotfiske <elliotfiske@gmail.com>
2016-01-30 00:30:00 -0800

    Working on dumb background

---
+++ Assets/OUR STUFF/Scripts/BackgroundManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+
+	public GameObject bgTemplate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
