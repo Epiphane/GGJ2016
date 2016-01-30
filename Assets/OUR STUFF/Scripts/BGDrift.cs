87846a2011a640bcc99df99b658b85d6a814455b
elliotfiske <elliotfiske@gmail.com>
2016-01-30 01:02:05 -0800

    Trippy background M80

---
+++ Assets/OUR STUFF/Scripts/BGDrift.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGDrift : MonoBehaviour {
+
+	public Vector3 basePosn;
+	float offset = 0.0f;
+
+	void Start() {
+		offset = Random.Range (-4.0f, 4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var newX = Mathf.Sin (Time.time + offset);
+		var newY = Mathf.Cos (Time.time + offset);
+		transform.localPosition = new Vector3 (newX, newY, 8.0f) + basePosn;
+		transform.RotateAround (transform.position, Vector3.forward, offset * 0.1f);
+	}
+}
--- Assets/OUR STUFF/Scripts/BackgroundManager.cs
+++ Assets/OUR STUFF/Scripts/BackgroundManager.cs
@@ -7,16 +7,17 @@ public class BackgroundManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int X_MAX = 5;
-		int Y_MAX = 5;
-		for (int x = 0; x < 4; x++) {
-			for (int y = 0; y < 4; y++) {
+		int X_MAX = 3;
+		int Y_MAX = 3;
+		for (int x = 0; x < X_MAX; x++) {
+			for (int y = 0; y < Y_MAX; y++) {
 				var newBG = GameObject.Instantiate (bgTemplate);
-				newBG.GetComponent<SpriteRenderer> ().color = new Color (Random.Range(0.0f, 0.2f), Random.Range(0.0f, 0.2f), Random.Range(0.0f, 0.2f));
+				newBG.GetComponent<SpriteRenderer> ().color = new Color (Random.Range(0.0f, 0.1f), Random.Range(0.16f, 0.5f), Random.Range(0.1f, 0.5f));
 
 				var newX = Random.Range (-3.0f, 3.0f);
 				var newY = Random.Range (-3.3f, 3.3f);
-				newBG.transform.position = new Vector2 (newX, newY);
+				newBG.GetComponent<BGDrift>().basePosn = new Vector3 (-3.0f + x * 2.0f, -3.0f + y * 2.0f, 8.0f);
+				newBG.transform.RotateAround (transform.position, Vector3.forward, Random.Range (-90.0f, 90.0f));
 
 				newBG.transform.parent = this.transform;
 			}
