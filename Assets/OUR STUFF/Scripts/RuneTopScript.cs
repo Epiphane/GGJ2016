f9a22052945968e75d6dd241d50f0ca934a6eafb
Thomas Steinke <exyphnos@gmail.com>
2016-01-31 13:13:18 -0800

    Rune Dude

---
+++ Assets/OUR STUFF/Scripts/RuneTopScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuneTopScript : MonoBehaviour {
+
+	public Sprite[] frames;
+
+	void Set(int sprite) {
+		GetComponent<SpriteRenderer> ().sprite = frames [sprite];
+	}
+}
