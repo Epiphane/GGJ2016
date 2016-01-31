a48bcac92cb4e835d189c7423747051a9e66cd83
Thomas Steinke <exyphnos@gmail.com>
2016-01-31 15:32:02 -0800

    Ruuuunes

--- Assets/OUR STUFF/Scripts/GateScript.cs
+++ Assets/OUR STUFF/Scripts/GateScript.cs
@@ -14,14 +14,23 @@ public class GateScript : MonoBehaviour {
 		}
 	}
 
+	private int unlockedGates = 0;
+
 	public void GainSoul() {
 		Debug.Log ("Got a soul");
 		bool didCoolThing = runeTop.Gain (1);
+
+		if (didCoolThing && unlockedGates < 8) {
+			GameObject.Find ("Rune " + unlockedGates).GetComponent<RuneScript>().enabled = true; 
+			unlockedGates++;
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		for (int i = 0; i < 8; i++) {
+			GameObject.Find ("Rune " + i).GetComponent<RuneScript> ().enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/OUR STUFF/Scripts/RuneScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1, 0);
+	}
+
+	private float fade = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (fade < 1) {
+			fade = Mathf.Min (fade + Time.deltaTime, 1);
+
+			GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1, fade);
+		}
+	}
+}
