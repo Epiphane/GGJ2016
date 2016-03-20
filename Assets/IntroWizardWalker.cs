bd7c28e6877c28e24d9fbf4a480e839e3ea0e8db
elliotfiske <elliotfiske@gmail.com>
2016-03-19 21:22:57 -0700

    Intro stufF

---
+++ Assets/IntroWizardWalker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroWizardWalker : MonoBehaviour {
+
+	public Sprite second_bg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey && !Input.GetMouseButton(0)) {
+			SceneManager.LoadScene ("ElliotMinigame");
+		}
+	
+	}
+
+	public void WizardWalkEnded() {
+		GameObject.Find("bg").GetComponent<SpriteRenderer>().sprite = second_bg;
+		GameObject.Find ("mountains").SetActive (false);
+		this.gameObject.SetActive (false);
+		WizWalkToArena.IsWalking = true;
+
+		GameObject.Find ("wizz_red").GetComponent<Animator> ().SetTrigger ("do_drama");
+		GameObject.Find ("wizz_blue").GetComponent<Animator> ().SetTrigger ("do_drama");
+
+		GameObject.Find ("TITLE").GetComponent<Animator> ().SetTrigger ("title_start");
+		GameObject.Find ("TITLE").GetComponent<Text> ().color = Color.white;
+	}
+}
---
+++ Assets/WizWalkToArena.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizWalkToArena : MonoBehaviour {
+
+	public float targetX = 0;
+	public float targetY = -1.35f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsWalking) {
+			Vector3 diff_vec = (new Vector3(targetX, targetY, 0)) - this.transform.position;
+
+			if (diff_vec.magnitude <= 0.2f) {
+				this.gameObject.SetActive (false);
+			}
+
+			diff_vec.Normalize ();
+
+			this.transform.position += diff_vec * 0.02f;
+
+		}
+	}
+
+	public static bool IsWalking = false;
+}
